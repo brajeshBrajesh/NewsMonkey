ef732f2d953c6b5feb4ca65f6901871b0912f0f6
brajeshBrajesh <singh.brajesh8189250@gmail.com>
2021-12-22 23:18:39 +0530

    added next and prev (vecdio-28)

--- src/components/News.js
+++ src/components/News.js
@@ -3,40 +3,40 @@ import NewsItem from "./NewsItem";
 
 export class news extends Component {
   articles = [
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: {
-        id: "espn-cric-info",
-        name: "ESPN Cric Info",
-      },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
+    // {
+    //   source: {
+    //     id: "espn-cric-info",
+    //     name: "ESPN Cric Info",
+    //   },
+    //   author: null,
+    //   title:
+    //     "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
+    //   description:
+    //     "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
+    //   url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
+    //   urlToImage:
+    //     "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
+    //   publishedAt: "2020-04-27T11:41:47Z",
+    //   content:
+    //     "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
+    // },
+    // {
+    //   source: {
+    //     id: "espn-cric-info",
+    //     name: "ESPN Cric Info",
+    //   },
+    //   author: null,
+    //   title:
+    //     "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+    //   description:
+    //     "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+    //   url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+    //   urlToImage:
+    //     "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+    //   publishedAt: "2020-03-30T15:26:05Z",
+    //   content:
+    //     "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
+    // },
   ];
   constructor() {
     super();
@@ -44,11 +44,62 @@ export class news extends Component {
     this.state = {
       articles: this.articles,
       loading: false,
-
-      
     };
     console.log(this.articles);
   }
+  async componentDidMount() {
+    console.log("ComponentDidMount");
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=50a60f7ef59e488f81c23e2a2574e94c&page=1&pageSize=20";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    let disablePrev = true;
+    let disableNext = false;
+    let numPages = Math.ceil(parsedData.totalResults / 20);
+    if (numPages == 1) disableNext = true;
+    this.setState({
+      articles: parsedData.articles,
+      page: 1,
+      disableNext: disableNext,
+      disablePrev: disablePrev,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("Prev click");
+    let page = this.state.page - 1;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=50a60f7ef59e488f81c23e2a2574e94c&page=${page}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    let disablePrev = false;
+    let disableNext = false;
+    if (page == 1) disablePrev = true;
+    this.setState({
+      articles: parsedData.articles,
+      page: page,
+      disableNext: disableNext,
+      disablePrev: disablePrev,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("Next click ");
+    let page = this.state.page + 1;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=50a60f7ef59e488f81c23e2a2574e94c&page=${page}&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    let disablePrev = false;
+    let disableNext = false;
+    let numPages = Math.ceil(parsedData.totalResults / 20);
+    if (page >= numPages) disableNext = true;
+    this.setState({
+      articles: parsedData.articles,
+      page: page,
+      disableNext: disableNext,
+      disablePrev: disablePrev,
+    });
+  };
+
   render() {
     return (
       <>
@@ -56,20 +107,46 @@ export class news extends Component {
           <h1 style={{ textAlign: "center" }} className="my-4">
             NewsMonkey-Top Headlines
           </h1>
-          <div className="row my-4">
+          <div className="row">
             {this.state.articles.map((element) => {
               return (
-                <div className="col-md-4 my-2 mx-2" key={element.url}>
+                <div className="col-md-4" key={element.url}>
                   <NewsItem
-                    title={element.title.slice(0,45)}
-                    description={element.description.slice(0,130)}
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 130)
+                        : ""
+                    }
                     newsUrl={element.url}
-                    imageUrl={element.urlToImage}
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
+                    }
                   />
                 </div>
               );
             })}
           </div>
+        </div>
+        <div className="container my-2 d-flex justify-content-between">
+          <button
+            onClick={this.handlePrevClick}
+            type="button"
+            className="btn btn-dark"
+            disabled={this.state.disablePrev}
+          >
+            &larr; Prev
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+            disabled={this.state.disableNext}
+          >
+            Next &rarr;
+          </button>
         </div>
       </>
     );
